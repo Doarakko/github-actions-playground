1fec5f514720b285cc4715e8d1a6172dba2b5f32
peperoncino <2wua4nlyi@gmail.com>
2021-10-30 13:10:57 +0900

    add draw sample

--- poetry-project/main.py
+++ poetry-project/main.py
@@ -1,1 +1,5 @@
-print("hello world")
+import requests
+
+r = requests.get("https://db.ygoprodeck.com/api/v7/randomcard.php").json()
+
+print("Draw \"{}\"\n{}".format(r["name"], r["card_images"][0]["image_url"]))
